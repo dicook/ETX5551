926251290461e47d5311356cea39bb129ea797bc
dicook <visnut@gmail.com>
2025-05-20 11:20:41 +1000

    small block of code for today's class

---
+++ class3.R
@@ -0,0 +1,121 @@
+# Code for class 3
+library(tidyverse)
+library(tourr)
+library(mulgar)
+library(mclust)
+library(detourr)
+library(ggthemes)
+library(colorspace)
+library(crosstalk)
+library(plotly)
+library(viridis)
+
+load("data/penguins_sub.rda")
+
+# Model-based clustering
+penguins_BIC <- mclustBIC(penguins_sub[,1:4])
+ggmc <- ggmcbic(penguins_BIC, cl=2:9, top=7) + 
+  scale_color_discrete_divergingx(palette = "Roma") +
+  theme_minimal() 
+ggmc
+
+# Generate ellipses
+penguins_mc <- Mclust(penguins_sub[,1:4], 
+                      G=4, 
+                      modelNames = "VEE")
+penguins_mce <- mc_ellipse(penguins_mc)
+penguins_cl <- penguins_sub
+penguins_cl$cl <- factor(penguins_mc$classification)
+
+# Examine the four cluster solution
+penguins_mc <- Mclust(penguins_sub[,1:4], 
+                      G=4, 
+                      modelNames = "VEE")
+penguins_mce <- mc_ellipse(penguins_mc)
+penguins_cl <- penguins_sub
+penguins_cl$cl <- factor(penguins_mc$classification)
+
+penguins_mc_data <- penguins_cl |>
+  select(bl:bm, cl) |>
+  mutate(type = "data") |>
+  bind_rows(bind_cols(penguins_mce$ell,
+                      type=rep("ellipse",
+                               nrow(penguins_mce$ell)))) |>
+  mutate(type = factor(type))
+
+animate_xy(penguins_mc_data[,1:4],
+           col=penguins_mc_data$cl,
+           pch=c(4, 20 )[as.numeric(penguins_mc_data$type)], 
+           axes="off")
+
+# Examine the three cluster solution
+penguins_mc <- Mclust(penguins_sub[,1:4], 
+                      G=3, 
+                      modelNames = "EEE")
+penguins_mce <- mc_ellipse(penguins_mc)
+penguins_cl <- penguins_sub
+penguins_cl$cl <- factor(penguins_mc$classification)
+
+penguins_mc_data <- penguins_cl |>
+  select(bl:bm, cl) |>
+  mutate(type = "data") |>
+  bind_rows(bind_cols(penguins_mce$ell,
+                      type=rep("ellipse",
+                               nrow(penguins_mce$ell)))) |>
+  mutate(type = factor(type))
+
+animate_xy(penguins_mc_data[,1:4],
+           col=penguins_mc_data$cl,
+           pch=c(4, 20 )[as.numeric(penguins_mc_data$type)], 
+           axes="off")
+
+# Comparing solutions
+p_dist <- dist(penguins_sub[,1:4])
+p_hcw <- hclust(p_dist, method="ward.D2")
+p_cl <- data.frame(cl_w = cutree(p_hcw, 3))
+
+penguins_mc <- Mclust(penguins_sub[,1:4], 
+                      G=3, 
+                      modelNames = "EEE")
+p_cl <- p_cl |> 
+  mutate(cl_mc = penguins_mc$classification)
+
+p_cl |> 
+  count(cl_w, cl_mc) |> 
+  pivot_wider(names_from = cl_mc, 
+              values_from = n, 
+              values_fill = 0) 
+
+p_cl <- p_cl |> 
+  mutate(cl_w_j = jitter(cl_w),
+         cl_mc_j = jitter(cl_mc))
+penguins_cl <- bind_cols(penguins_sub, p_cl)
+p_cl_shared <- SharedData$new(penguins_cl)
+
+set.seed(1046)
+detour_plot <- detour(p_cl_shared, tour_aes(
+  projection = bl:bm,
+  colour = cl_mc)) |>
+  tour_path(grand_tour(2), 
+            max_bases=100, fps = 60) |>
+  show_scatter(alpha = 0.7, axes = FALSE,
+               width = "100%", height = "450px")
+
+conf_mat <- plot_ly(p_cl_shared, 
+                    x = ~cl_mc_j,
+                    y = ~cl_w_j,
+                    color = ~cl_mc,
+                    colors = viridis_pal(option = "D")(3),
+                    height = 450) |>
+  highlight(on = "plotly_selected", 
+            off = "plotly_doubleclick") |>
+  add_trace(type = "scatter", 
+            mode = "markers")
+
+bscols(
+  detour_plot, conf_mat,
+  widths = c(5, 6)
+)                 
+
+# Clustering a blob
+# See the Fritz Leisch slides
